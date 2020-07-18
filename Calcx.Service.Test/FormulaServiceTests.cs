6fe9ac50768b332acd4dcc73f962e2b54eb59331
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-07-18 10:33:00 +0100

    xUnit test added

---
+++ Calcx.Service.Test/FormulaServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Calcx.Service.Test
+{
+    public class FormulaServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

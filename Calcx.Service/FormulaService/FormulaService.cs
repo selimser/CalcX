bd96f4cc2cf799893bc399a7be853ebd383b2179
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-07-18 10:02:27 +0100

    formula generation initial implementation for single and collection of LaTeX formulæ

--- Calcx.Service/FormulaService/FormulaService.cs
+++ Calcx.Service/FormulaService/FormulaService.cs
@@ -1,31 +1,73 @@
 ﻿using CSharpMath.SkiaSharp;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Calcx.Service.FormulaService
 {
     public class FormulaService : IFormulaService
     {
-        public string TestMethod()
+        public async Task<string> GenerateFormulaAsBase64(string latexExpression, float painterCanvasWidth = 1024, float fontSize = 14)
         {
             var painter = new MathPainter()
             {
-                LaTeX = @"F(x) &= \int^a_b \frac{1}{3}x^3"
+                LaTeX = latexExpression,
+                FontSize = fontSize
             };
 
-            var imageStream = painter.DrawAsStream(format: SkiaSharp.SKEncodedImageFormat.Png, textPainterCanvasWidth: 250 );
-
-            byte[] arrayData;
             using (var memoryStream = new MemoryStream())
             {
-                imageStream.CopyTo(memoryStream);
-                arrayData = memoryStream.ToArray();
+                var imageStream = painter.DrawAsStream
+                (
+                    format: SkiaSharp.SKEncodedImageFormat.Png,
+                    textPainterCanvasWidth: painterCanvasWidth
+                );
+
+                await imageStream.CopyToAsync(memoryStream);
+
+                var base64String = Convert.ToBase64String(memoryStream.ToArray());
+
+                return base64String;
+            }
+        }
+
+        public async Task<Dictionary<string, string>> GenerateMultipleFormulaAsBase64(Dictionary<string, string> formulaCollection, float painterCanvasWidth = 1024, float fontSize = 14)
+        {
+            var painter = new MathPainter()
+            {
+                FontSize = fontSize
+            };
+
+            Dictionary<string, string> returnCollection = null;
+            if (formulaCollection.Keys.Any())
+            {
+                returnCollection = new Dictionary<string, string>();
+
+                foreach (var formula in formulaCollection)
+                {
+                    painter.LaTeX = formula.Value;
+
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        var imageStream = painter.DrawAsStream
+                        (
+                            format: SkiaSharp.SKEncodedImageFormat.Png,
+                            textPainterCanvasWidth: painterCanvasWidth
+                        );
+
+                        await imageStream.CopyToAsync(memoryStream);
+
+                        var base64String = Convert.ToBase64String(memoryStream.ToArray());
+
+                        returnCollection.Add(formula.Key, base64String);
+                    }
+                }
             }
 
-            var base64String = Convert.ToBase64String(arrayData);
-
-            return base64String;
+            return returnCollection;
         }
     }
 }
--- Calcx.Service/FormulaService/IFormulaService.cs
+++ Calcx.Service/FormulaService/IFormulaService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Calcx.Service.FormulaService
 {
     public interface IFormulaService
     {
-        string TestMethod();
+        Task<string> GenerateFormulaAsBase64(string latexExpression, float painterCanvasWidth = 1024, float fontSize = 14);
+        Task<Dictionary<string, string>> GenerateMultipleFormulaAsBase64(Dictionary<string, string> formulaCollection, float painterCanvasWidth = 1024, float fontSize = 14);
     }
 }

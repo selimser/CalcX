f7ce4d0372ad5c61249208b41cf65d3967fa49c5
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-08-26 11:51:39 +0100

    temp

---
+++ Calcx.Core/Entity/DegreeMinuteSecond.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcx.Core.Entity
+{
+    public sealed class DegreeMinuteSecond
+    {
+        public double Degrees { get; internal set; }
+        public double Minutes { get; internal set; }
+        public double Seconds { get; internal set; }
+        public string GetFormattedString()
+        {
+            return $"{Degrees}{Convert.ToChar("0xB0")} {Minutes}\" {Seconds}'";
+        }
+
+        public string GetLaTeXString() //TODO: verify this.
+        {
+            return Degrees + @"^{\circ} " + $"{Minutes}\" {Seconds}'";
+        }
+
+
+    }
+}
